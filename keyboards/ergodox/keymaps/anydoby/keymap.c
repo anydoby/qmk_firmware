8811639a22236533e6bb17a4b79c0a2b7b9e3fd1
Sergey Zolotaryov <szolotarov@asset-control.com>
2017-02-27 14:15:03 +0100

    added default qwerty layer with proper latin code for the cyrillic X character. the top left button is designated for hard sign. letter 'yo' can be entered using the symbols layer

--- keyboards/ergodox/keymaps/anydoby/keymap.c
+++ keyboards/ergodox/keymaps/anydoby/keymap.c
@@ -7,6 +7,7 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 #define NAVI  3 // navigation and cursor keys
+#define QWERTY 4  // standard QWERTY layout for russian input
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -21,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   F2 |   F3 |   F4 |  F5  |   F6 | 0    |           | 1    |   F7 |  F8  |  F9  |  F10 |  F11 |  F12   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Del    |   Q  |   W  |   E  |   R  |   T  |  L5  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | BkSp   | Alt/A|Ctrl/S|LSft/D|   F  |   G  |------|           |------|   H  |   J  | K/L3 | L/L2 |   ;  |' / Cmd |
  * |--------+------+------+------+------+------| Hyper|           | L3   |------+------+------+------+------+--------|
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_0,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(QWERTY),
         KC_BSPC,        ALT_T(KC_A),    CTL_T(KC_S),  SFT_T(KC_D),   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
 		MO(SYMB),KC_LGUI,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
@@ -183,6 +184,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TAB,KC_ENTER
 ),
+/* Keymap 5: QWERTY layer for cyrillic input
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   Ъ    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Del    |   Q  |   W  |   E  |   R  |   T  |  L0  |           |  L0  |   Y  |   U  |   I  |   O  |   P  |   Х    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
+ *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[QWERTY] = KEYMAP(  // layer 5 : default qwerty
+        // left hand
+        KC_RBRACKET,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
+        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+                                              ALT_T(KC_APP),  KC_LGUI,
+                                                              KC_HOME,
+                                               KC_SPC,KC_BSPC,KC_END,
+        // right hand
+             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_TRNS,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRACKET,
+                          KC_H,   KC_J,  KC_K,  LT(NAVI, KC_L),   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
+             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+             KC_LALT,        CTL_T(KC_ESC),
+             KC_PGUP,
+             KC_PGDN,KC_TAB, KC_ENT
+    ),
 
 };
 
@@ -259,6 +301,10 @@ void matrix_scan_user(void) {
             ergodox_right_led_2_on();
             break;
         case NAVI:
+        	ergodox_right_led_3_on();
+        	break;
+        case QWERTY:
+        	ergodox_right_led_1_on();
         	ergodox_right_led_3_on();
         	break;
         default:
